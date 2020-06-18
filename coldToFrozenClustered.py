b68da86aee8189beac28903e80fb665d8cad8ce7
tmarrazzo <tony@keostech.com>
2020-06-17 18:44:51 -0700

    Cold to frozen script for clustered environments

--- coldToFrozenClustered.py
+++ coldToFrozenClustered.py
@@ -1,63 +1,76 @@
-# This is an example script for archiving cold buckets. It must be modified
-# to suit your individual needs, and we highly recommend testing this on a
-# non-production instance before deploying it.
+import sys
+import os
+import subprocess
+from datetime import datetime
+import socket
+import pipes
 
-import sys, os, gzip, shutil, subprocess, random
-
-### CHANGE THIS TO YOUR ACTUAL ARCHIVE DIRECTORY!!!
-ARCHIVE_DIR = os.path.join(os.getenv('SPLUNK_HOME'), 'frozenarchive')
+# Change the below settings as appropriate for your environment
+ARCHIVE_DIR = '/splunkfrozen'
+REMOTE_SERVER = '192.168.32.133'
 
 # For new style buckets (v4.2+), we can remove all files except for the rawdata.
 # We can later rebuild all metadata and tsidx files with "splunk rebuild"
-def handleNewBucket(base, files):
+
+
+def strip_bucket(base, files):
     print 'Archiving bucket: ' + base
     for f in files:
         full = os.path.join(base, f)
         if os.path.isfile(full):
             os.remove(full)
 
-# For buckets created before 4.2, simply gzip the tsidx files
-# To thaw these buckets, be sure to first unzip the tsidx files
-def handleOldBucket(base, files):
-    print 'Archiving old-style bucket: ' + base
-    for f in files:
-        full = os.path.join(base, f)
-        if os.path.isfile(full) and (f.endswith('.tsidx') or f.endswith('.data')):
-            fin = open(full, 'rb')
-            fout = gzip.open(full + '.gz', 'wb')
-            fout.writelines(fin)
-            fout.close()
-            fin.close()
-            os.remove(full)
 
-# This function is not called, but serves as an example of how to do
-# the previous "flatfile" style export. This method is still not
-# recommended as it is resource intensive
-def handleOldFlatfileExport(base, files):
-    command = ['exporttool', base, os.path.join(base, 'index.export'), 'meta::all']
-    retcode = subprocess.call(command)
-    if retcode != 0:
-        sys.exit('exporttool failed with return code: ' + str(retcode))
+def check_remote_path(host, dirpath):
+    """
+    Check if a path is valid on a remote host
+    :param host: Remote server
+    :param path: Path to test
+    :return:
+    """
+    try:
+        status = subprocess.call(
+            ['ssh', host, 'test -d {}'.format(pipes.quote(dirpath))])
+        if status == 0:
+            return True
+        if status == 1:
+            return False
+    except Exception as ex:
+        raise ex
 
-    for f in files:
-        full = os.path.join(base, f)
-        if os.path.isfile(full):
-            os.remove(full)
-        elif os.path.isdir(full):
-            shutil.rmtree(full)
-        else:
-            print 'Warning: found irregular bucket file: ' + full
+
+def create_remote_dir(host, path):
+    """
+    Create recursive path on remote host
+    :param host: Remote server
+    :param path: Path to create
+    :return:
+    """
+    try:
+        subprocess.call(['ssh', host, 'mkdir -p ', path])
+    except Exception as ex:
+        raise ex
+
+
+def copy_to_frozen(host, source, target):
+    """
+    Copy cold bucket to frozen path on remote server
+    :param host: Target host to copy to
+    :param source: Source path to copy from
+    :param target: Target path to copy into
+    :return:
+    """
+    cmd = "scp -rq " + source + " " + host + ":" + target
+    print cmd
+    try:
+        os.system(cmd)
+    except Exception as ex:
+        raise ex
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        sys.exit('usage: python coldToFrozenExample.py <bucket_dir_to_archive>')
-
-    if not os.path.isdir(ARCHIVE_DIR):
-        try:
-            os.mkdir(ARCHIVE_DIR)
-        except OSError:
-            # Ignore already exists errors, another concurrent invokation may have already created this dir
-            sys.stderr.write("mkdir warning: Directory '" + ARCHIVE_DIR + "' already exists\n")
+        sys.exit('usage: python coldToFrozenClustered.py <bucket_dir_to_archive>')
 
     bucket = sys.argv[1]
     if not os.path.isdir(bucket):
@@ -67,22 +80,18 @@ if __name__ == "__main__":
     if not os.path.isdir(rawdatadir):
         sys.exit('No rawdata directory, given bucket is likely invalid: ' + bucket)
 
+    frozenbucket = os.path.basename(bucket).replace("rb_", "db_")  # get bucket name and swap rb with db
+    indexname = os.path.basename(os.path.dirname(os.path.dirname(bucket)))  # get index for building /ARCHIVE/<INDEX>/
+    hostname = socket.gethostname()  # get hostname for building /ARCHIVE/INDEX/<HOST>
+    destbucket = os.path.join(ARCHIVE_DIR, indexname, hostname, frozenbucket)
+
+    # Test to see if this bucket already exists on the remote server
+    if check_remote_path(REMOTE_SERVER, destbucket):
+        exit(0)  # Bucket already exists, ret 0 and let Splunk kill the bucket
+    else:
+        create_remote_dir(REMOTE_SERVER, destbucket)  # Bucket does not exist, create /ARCHIVE/INDEX/HOST/BUCKET
+
     files = os.listdir(bucket)
     journal = os.path.join(rawdatadir, 'journal.gz')
-    if os.path.isfile(journal):
-        handleNewBucket(bucket, files)
-    else:
-        handleOldBucket(bucket, files)
-
-    if bucket.endswith('/'):
-        bucket = bucket[:-1]
-
-    indexname = os.path.basename(os.path.dirname(os.path.dirname(bucket)))
-    destdir = os.path.join(ARCHIVE_DIR, indexname, os.path.basename(bucket))
-
-    while os.path.isdir(destdir):
-        print 'Warning: This bucket already exists in the archive directory'
-        print 'Adding a random extension to this directory...'
-        destdir += '.' + str(random.randrange(10))
-
-    shutil.copytree(bucket, destdir)
+    strip_bucket(bucket, files)  # Delete all files in bucket except rawdata dir contents
+    copy_to_frozen(REMOTE_SERVER, rawdatadir, destbucket)  # Copy 'rawdata' dir to /ARCHIVE/INDEX/HOST/BUCKET
